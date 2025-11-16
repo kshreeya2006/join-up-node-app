06fe418bcac2c28c43b3ce6fe01444e3e4bd8fef
kshreeya2006 <kshreeya2006@gmail.com>
2025-11-16 20:15:48 +0530

    update changes to index.js and vercel.json

--- index.js
+++ index.js
@@ -3,68 +3,27 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import userRouter from "./routes/userRoutes.js";
-import productRouter from "./routes/productRoutes.js";
-import orderRouter from "./routes/orderRoutes.js";
-import dotenv from 'dotenv';
+import eventRouter from "./routes/eventRoutes.js";
+import registrationRouter from "./routes/registrationRoutes.js";
+
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const MONGODB_URI= process.env.MONGODB_URI
-
-//In case not connecting to backend APIs via the url
-//const DBUSER= encodeURIComponenet(process.env.DBUSER);
-//const DBPASS= encodeURIComponenet(process.env.DBPASS);
-//const MONGODB_URI= `mongodb+srv://${DBUSER}:${DBPASS}@cluster0.d874fm8.mongodb.net/gcet?retryWrites=true&w=majority&appName=Cluster0`
-
+// Routes
 app.use("/users", userRouter);
+app.use("/events", eventRouter);
+app.use("/registrations", registrationRouter);
 
-app.use("/products", productRouter);
-
-app.use("/orders", orderRouter);
+// MongoDB connection
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
   .connect(MONGODB_URI)
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server Started on port 8080");
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log("DB Error:", error));
 
-
-export default app;
-
-
-/**app.get("/", (req, res)=>{
-  return res.send(`<h1>Welcome to the API Index</h1>
-    <ol>
-      <li><a href="/greet">/greet</a></li>
-      <li><a href="/name">/name</a></li>
-      <li><a href="/weather">/weather</a></li>
-      <li><a href="/products">/products</a></li>
-      <li><a href="/register">/register</a></li>
-      <li><a href="/login">/login</a></li>
-    </ol>
-  `);
-   res.send("Good Morning!!");
-});
-
-app.get("/greet", (req, res)=>{
-  res.send("Greetings!!");
-} );
-
-app.get("/name", (req, res)=>{
-  res.send("Shreeya");
-} );
-
-app.get("/weather", (req, res)=>{
-  res.send("29 degrees");
-});
-
-**/
-
+export default app;  
